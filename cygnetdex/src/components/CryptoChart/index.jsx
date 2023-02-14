0e5b541b96b37c4b3e752647398669ae5a15455d
Paul <pajmurray@outlook.com>
2023-02-14 10:28:47 +0000

    Update

--- cygnetdex/src/components/CryptoChart/index.jsx
+++ cygnetdex/src/components/CryptoChart/index.jsx
@@ -1,214 +1,68 @@
-import "./styles.scss";
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { Line } from "react-chartjs-2";
-import {
-  CircularProgress,
-  createTheme,
-  makeStyles,
-  ThemeProvider,
-} from "@material-ui/core";
-import Chart from "chart.js/auto";
+import React, { useState, useEffect } from "react";
+import TradingViewWidget from "react-tradingview-widget";
+import cors from "cors";
 
-export const CryptoChart = (props) => {
-  const [cryptoTimeData, setCryptoTimeData] = useState([]);
-  const [xrpTimeData, setXrpTimeData] = useState([]);
-  const [days, setDays] = useState("60");
-  const [currency, setCurrency] = useState("XRSWAN");
+const CryptoChart = () => {
+  const [data, setData] = useState([]);
 
-  const useStyles = makeStyles((theme) => ({
-    container: {
-      width: "75%",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 25,
-      padding: 40,
-      [theme.breakpoints.down("md")]: {
-        width: "100%",
-        marginTop: 0,
-        padding: 20,
-        paddingTop: 0,
-      },
-    },
-  }));
-
-  const classes = useStyles();
-
-  const fetchCryptoStats = async () => {
-    try {
-      let response = await fetch(
-        `https://api.onthedex.live/public/v1/ohlc?base=XRSWAN.rUHoc9nQyZzahE5rq26QHkvBmnvCEoPAhZ&quote=XRP&bars=100&interval=60`
-      );
-      let data = await response.json();
-      console.log("cryptoStats", data);
-      setCryptoTimeData(data.data.ohlc);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const fetchXrpStats = async () => {
-    try {
-      let response = await fetch(
-        `https://api.onthedex.live/public/v1/ohlc?base=XRP&quote=XRSWAN.rUHoc9nQyZzahE5rq26QHkvBmnvCEoPAhZ&bars=100&interval=60`
-      );
-      let data = await response.json();
-      console.log("xrp?", data);
-      setXrpTimeData(data.data.ohlc);
-    } catch (error) {
-      console.log(error);
-    }
+  const options = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
   };
 
   useEffect(() => {
-    fetchCryptoStats();
-    fetchXrpStats();
-  }, []); // Dependencies will be days and currencies
+    try {
+      async function fetchData() {
+        const response = await fetch(
+          "https://api.onthedex.live/public/v1/ohlc?base=CSC.rCSCManTZ8ME9EoLrSHHYKW8PPwWMgkwr&quote=XRP&bars=100&interval=60",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "http://localhost:3000",
+            },
+          }
+        );
+        const json = await response.json();
+        setData([json]);
+      }
+      fetchData();
+      console.log("dataaa", data);
+    } catch (error) {
+      console.log("fetchData error", error);
+    }
+  }, []);
 
-  const darkTheme = createTheme({
-    palette: {
-      primary: {
-        main: "#fff",
-      },
-      type: "dark",
-    },
+  let base = "BTC";
+  console.log("!!!!!!!!base", base);
+  let quote = data[0].spec?.quote;
+  let symbol = `CRYPTOCAP:${base}/${quote}`;
+  const reformattedData = data.map((point) => {
+    return {
+      time: point.t,
+      open: point.o,
+      high: point.h,
+      low: point.l,
+      close: point.c,
+      volume: point.vb,
+      quote_volume: point.vq,
+    };
   });
 
+  console.log(symbol);
   return (
-    <>
-      <ThemeProvider theme={darkTheme}>
-        <div className={classes.container}>
-          {!cryptoTimeData ? (
-            <CircularProgress
-              style={{ color: "gold" }}
-              size={250}
-              thickness={1}
-            />
-          ) : (
-            <>
-              <div className="chart1">
-                <Line
-                  data={{
-                    labels: cryptoTimeData.map((coin) => {
-                      // console.log(coin)
-                      let unixTimestamp = coin.t;
-                      let date = new Date(unixTimestamp * 1000);
-                      let days = 60;
-                      let hours = date.getHours();
-                      let minutes = "0" + date.getMinutes();
-                      let seconds = "0" + date.getSeconds();
-                      let formattedTime =
-                        hours +
-                        ":" +
-                        minutes.substr(-2) +
-                        ":" +
-                        seconds.substr(-2);
-                      // console.log(formattedTime);
-                      let time =
-                        date.getHours() > 12
-                          ? `${date.getHours() - 12}:${date.getMinutes()} PM`
-                          : `${date.getHours()}:${date.getMinutes()} AM`;
-                      return days === 60
-                        ? formattedTime
-                        : date.toLocaleDateString();
-                    }),
-
-                    datasets: [
-                      {
-                        data: cryptoTimeData.map((coin) => coin.o),
-                        label: `Price ( Past ${days} Days ) in ${currency}`,
-                        borderColor: "#db0d1e",
-                      },
-                    ],
-                  }}
-                  options={{
-                    elements: {
-                      point: {
-                        radius: 1,
-                      },
-                    },
-                  }}
-                />
-              </div>
-              <div className="chart2">
-                {" "}
-                <Line
-                  data={{
-                    labels: xrpTimeData.map((coin) => {
-                      console.log(coin);
-                      let unixTimestamp = coin.t;
-                      let date = new Date(unixTimestamp * 1000);
-                      let days = 60;
-                      let hours = date.getHours();
-                      let minutes = "0" + date.getMinutes();
-                      let seconds = "0" + date.getSeconds();
-                      let formattedTime =
-                        hours +
-                        ":" +
-                        minutes.substr(-2) +
-                        ":" +
-                        seconds.substr(-2);
-                      console.log(formattedTime);
-                      let time =
-                        date.getHours() > 12
-                          ? `${date.getHours() - 12}:${date.getMinutes()} PM`
-                          : `${date.getHours()}:${date.getMinutes()} AM`;
-                      return days === 60
-                        ? formattedTime
-                        : date.toLocaleDateString();
-                    }),
-
-                    datasets: [
-                      {
-                        data: xrpTimeData.map((coin) => coin.c),
-                        label: `Price ( Past ${days} Days ) in XRP`,
-                        borderColor: "#EEBC1D",
-                      },
-                    ],
-                  }}
-                  options={{
-                    elements: {
-                      point: {
-                        radius: 1,
-                      },
-                    },
-                  }}
-                />
-              </div>
-
-              <div
-                style={{
-                  display: "flex",
-                  marginTop: 20,
-                  justifyContent: "space-around",
-                  width: "100%",
-                }}
-              >
-                {/* {chartDays.map((day) => (
-                  <SelectButton
-                    key={day.value}
-                    onClick={() => {
-                      setDays(day.value);
-                      setflag(false);
-                    }}
-                    selected={day.value === days}
-                  >
-                    {day.label}
-                  </SelectButton>
-                ))} */}
-              </div>
-            </>
-          )}
-        </div>
-      </ThemeProvider>
-    </>
+    <div>
+      {data.length > 0 && (
+        <TradingViewWidget
+          symbol={symbol}
+          interval="60"
+          data={reformattedData}
+          height={400}
+          width={1000}
+        />
+      )}
+    </div>
   );
 };
 
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CryptoChart);
+export default CryptoChart;
--- cygnetdex/src/pages/Home/index.jsx
+++ cygnetdex/src/pages/Home/index.jsx
@@ -3,12 +3,15 @@ import { connect } from "react-redux";
 import "./styles.scss";
 import ValueBar from "../../components/ValueBar";
 import OptionsPanel from "../../components/OptionsPanel";
-import CryptoGraph from "../../components/CryptoChart";
+import CryptoChart from "../../components/CryptoChart";
 import ClipLoader from "react-spinners/ClipLoader";
+import TradingViewChart from '../../components/TradingViewChart/TradingViewChart';
+
 <script
   defer
   src="https://www.livecoinwatch.com/static/lcw-widget.js"
 ></script>;
+
 export const Home = (props) => {
   const [xrswanValue, setXrswanValue] = useState([]);
   const [xrpValue, setXrpValue] = useState([]);
@@ -16,7 +19,7 @@ export const Home = (props) => {
   const [xrpTitle, setXrpTitle] = useState([]);
   const [xrswanTrend, setXrswanTrend] = useState([]);
   const [xrpTrend, setXrpTrend] = useState([]);
-  let [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   const fetchXrswanValues = async () => {
     try {
@@ -31,14 +34,14 @@ export const Home = (props) => {
         }
       );
       let response = await xrswanValues.json();
-      console.log("xrswan", response);
+      console.log("fetchXrswanValues", response);
       setXrswanTitle(response.pairs[0].base.currency);
       setXrswanValue(response.pairs[0].last);
       setXrswanTrend(response.pairs[0].trend);
       setLoading(false);
-      console.log(xrswanValue);
+      console.log('xrswanValue', xrswanValue);
     } catch (error) {
-      console.log(error);
+      console.log('fetchXrswanValues error', error);
     }
   };
   const fetchXrpValues = async () => {
@@ -53,68 +56,78 @@ export const Home = (props) => {
           },
         }
       );
-      let Rresponse = await xrpValues.json();
-      console.log("xrp", Rresponse);
-      // setXrpTitle(response.pairs[0].base.currency);
-      // setXrpValue(response.pairs[0].last);
-      // setXrpTrend(response.pairs[0].trend);
+      let response = await xrpValues.json();
+      console.log("fetchXrpValues", response);
+      setXrpTitle(response.pairs[0].base);
+      setXrpValue(response.pairs[0].last);
+      setXrpTrend(response.pairs[0].trend);
       setLoading(false);
       console.log(xrpValue);
     } catch (error) {
-      console.log(error);
+      console.log('fetchXrpValues error', error);
     }
   };
-
-  // fetchXrswanValues();
-  // fetchXrpValues();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       fetchXrswanValues();
       fetchXrpValues();
-    }, 134000);
+    }, 5000);
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <>
-      { loading === true ? <> <div className="loading">
-        <ClipLoader
-        color="red"
-        loading={loading}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-      </div> </> : <>
+      {loading === true ? (
+        <>
+          {" "}
+          <div className="loading">
+            <ClipLoader
+              color="red"
+              loading={loading}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>{" "}
+        </>
+      ) : (
+        <>
           <div className="main">
-              <div
-        class="livecoinwatch-widget-5"
-        lcw-base="USD"
-        lcw-color-tx="#999999"
-        lcw-marquee-1="coins"
-        lcw-marquee-2="movers"
-        lcw-marquee-items="10"
-      ></div>
-      {xrswanValue && xrswanTitle ? (
-        <ValueBar title={xrswanTitle} value={xrswanValue} trend={xrswanTrend} />
-      ) : (
-        "Loading"
-      )}
-      {xrpValue && xrpTitle ? (
-        <ValueBar title={xrpTitle} value={xrpValue} trend={xrpTrend}  className='value-bar' />
-      ) : (
-        "Loading"
-      )}
-      <OptionsPanel />
-      <CryptoGraph />
+            <div
+              class="livecoinwatch-widget-5"
+              lcw-base="USD"
+              lcw-color-tx="#999999"
+              lcw-marquee-1="coins"
+              lcw-marquee-2="movers"
+              lcw-marquee-items="10"
+            ></div>
+            {xrswanValue && xrswanTitle ? (
+              <ValueBar
+                title={xrswanTitle}
+                value={xrswanValue}
+                trend={xrswanTrend}
+              />
+            ) : (
+              "Loading"
+            )}
+            {xrpValue && xrpTitle ? (
+              <ValueBar
+                title={xrpTitle}
+                value={xrpValue}
+                trend={xrpTrend}
+                className="value-bar"
+              />
+            ) : (
+              "Loading"
+            )}
+            <OptionsPanel />
+              <CryptoChart />
+              {/* <TradingViewChart /> */}
           </div>
-           
-      </>}
-      
-      
-     
+        </>
+      )}
     </>
   );
 };
